9755c13b38502f6501b5d8f734a4d6c3455d88bf
Dheeraj Arora <118870441+darora9@users.noreply.github.com>
2024-07-17 18:27:10 +0530

    Update ShopContext.jsx

--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -26,14 +26,16 @@ const ShopContextProvider = (props) => {
         for(const item in cartItems){
             if(cartItems[item]>0) {
                 let itemInfo = all_product.find((product) =>product.id===Number(item));
-                totalAmount += itemInfo.new_price * cartItems[item];  } 
+                totalAmount += itemInfo.new_price * cartItems[item]; 
+            } 
         }
         return totalAmount;  }
     const getTotalCartItems = () => {
         let totalItem = 0;
         for(const item in cartItems)  {
             if(cartItems[item] > 0)     {
-                totalItem += cartItems[item];  }  
+                totalItem += cartItems[item];  
+            }  
         }
         return totalItem;  }
     const contextValue = {getTotalCartItems,getTotalCartAmount,all_product,cartItems,addToCart,removeFromCart};
